c547b3120cec9852a00936f800854e542bb37215
sashtgnv <alekstuganov@yandex.ru>
2025-01-15 10:55:02 +0300

    first launch

---
+++ src/main/java/com/example/messenger/controllers/LoginController.java
@@ -0,0 +1,12 @@
+package com.example.messenger.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/")
+    public String login(){
+        return "login";
+    }
+}
